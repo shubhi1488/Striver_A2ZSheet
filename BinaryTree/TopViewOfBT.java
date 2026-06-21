03b3df174c54e6f15d10b88238687ad0b8201842
Shubhi Agarwal <shubhi1488@gmail.com>
2026-06-21 19:27:28 +0530

    Add files via upload

---
+++ BinaryTree/TopViewOfBT.java
@@ -0,0 +1,54 @@
+package BinaryTree;
+import java.util.*;
+public class TopViewOfBT {
+    static class Pair<K,V>{
+        private K key;
+        private V value;
+        public Pair(K key,V value){
+            this.key=key;
+            this.value=value;
+        }
+        public K getKey(){
+            return key;
+        }
+        public V getValue(){
+            return value;
+        }
+    }
+    public static List<Integer> topView(TreeNode root){
+        List<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+        Map<Integer,Integer> map=new TreeMap<>();
+        Queue<Pair<TreeNode,Integer>>q=new LinkedList<>();
+        q.add(new Pair<>(root,0));
+        while(!q.isEmpty()){
+            Pair<TreeNode,Integer>it=q.poll();
+            TreeNode node=it.getKey();
+            int line=it.getValue();
+            if(!map.containsKey(line)){
+                map.put(line,node.data);
+            }
+            if(node.left!=null){
+                q.add(new Pair<>(node.left,line-1));
+            }
+            if(node.right!=null){
+                q.add(new Pair<>(node.right,line+1));
+            }
+        }
+        for(Integer value:map.values()){
+            ans.add(value);
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        TreeNode root=new TreeNode(1);
+        root.left=new TreeNode(2);
+        root.left.left=new TreeNode(4);
+        root.left.right=new TreeNode(5);
+        root.left.right.left=new TreeNode(6);
+        root.right=new TreeNode(3);
+        root.right.right=new TreeNode(7);
+        List result=topView(root);
+        System.out.println("Top view:"+result);
+    }
+}
