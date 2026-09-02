9d97a8745375b4380ed0021cbc57be19bc02762c
Shubhi Agarwal <shubhi1488@gmail.com>
2026-09-02 13:23:12 +0530

    Update RemoveDuplicates.java

--- Arrays/RemoveDuplicates.java
+++ Arrays/RemoveDuplicates.java
@@ -1,3 +1,4 @@
+#Brute Force-
 package Arrays;
 import java.util.*;
 public class RemoveDuplicates {
@@ -33,5 +34,33 @@ public class RemoveDuplicates {
         System.out.println(r1.remove(arr));
 
 
+    }
+}
+
+#Optimal Approach-
+package Arrays;
+import java.util.*;
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] arr){
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[j]!=arr[i]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of the array");
+        int n=sc.nextInt();
+        System.out.println("enter elements of the array");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("unique elements of the array is:"+removeDuplicates(arr));
     }
 }
